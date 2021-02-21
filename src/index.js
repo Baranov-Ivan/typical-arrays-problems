d75c647777fdefc6536caeb9481d18ba55075aa5
baranov-ivan <vano95by@gmail.com>
2021-02-21 16:11:36 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(Array.isArray(array) && array.length)
+        return array.reduce((minValue, currentValue) => currentValue < minValue ? minValue = currentValue : minValue, Infinity);
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(Array.isArray(array) && array.length)
+        return array.reduce((maxValue, currentValue) => currentValue > maxValue ? maxValue = currentValue : maxValue, -Infinity);
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(Array.isArray(array) && array.length && arguments.length > 0)
+        return array.reduce((sum, currentValue) => sum + currentValue, 0)/array.length;
+    return 0;
 }
